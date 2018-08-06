9570e505e8f02c964f92b6c4132049962c7b01ef
VidushiJain30 <jainvidushi30@gmail.com>
2018-08-06 19:45:57 +0530

    created a dynamic table

---
+++ game.js
@@ -0,0 +1,19 @@
+var id=0;
+function createT()
+{
+	var tb1=document.getElementById("tb1");
+	for (var r=0;r<5;r++)
+	{
+		var row=document.createElement("tr");
+		for (var c=0;c<5;c++)
+		{
+			var cell=document.createElement("td");
+			cell.width="20px";
+			cell.height="20px";
+			cell.id=id++;
+			row.appendChild(cell);
+			cell.innerHTML=id;			
+		}
+		tb1.appendChild(row);
+	}
+}
