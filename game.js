f2a276647e8bf7b9356fde7e14aac76a64110e39
VidushiJain30 <jainvidushi30@gmail.com>
2018-08-07 18:40:32 +0530

    timer added

--- game.js
+++ game.js
@@ -1,26 +1,22 @@
-/*var id=0;
-function createT()
-{
-	var tb1=document.getElementById("tb1");
-	for (var r=0;r<5;r++)
-	{
-		var row=document.createElement("tr");
-		for (var c=0;c<5;c++)
-		{
-			var cell=document.createElement("td");
-			cell.width="20px";
-			cell.height="20px";
-			cell.id=id++;
-
-			row.appendChild(cell);
-			cell.innerHTML=id;			
-		}
-		tb1.appendChild(row);
-	}
-}*/
 function content(elem)
 {
 	//elem.style.backgroundColor="red";
 	var p=document.getElementById("para");
 	p.innerHTML='You clicked on cell:'+ ' ' + elem.innerText;
 }
+var ts=60*1;
+var min=parseInt(ts/60);
+var sec=parseInt(ts%60);
+function CheckTime(){
+	document.getElementById("timer").innerHTML='Time Left: '+ min+ ' : ' + sec;
+	if(ts<=0){
+		document.getElementById("timer").innerHTML='TIME OVER';		
+	}	
+	else{
+		ts=ts-1;
+		min=parseInt(ts/60);
+		sec=parseInt(ts%60);
+		setTimeout("CheckTime()",1000);
+	}
+}
+	setTimeout("CheckTime()",1000);
