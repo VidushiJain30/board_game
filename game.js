41faa7327c5c5eb969e13663f9e8c6f5a6b33c73
VidushiJain30 <jainvidushi30@gmail.com>
2018-08-07 22:15:21 +0530

    random data generated

--- game.js
+++ game.js
@@ -20,3 +20,32 @@ function CheckTime(){
 	}
 }
 	setTimeout("CheckTime()",1000);
+
+var blocks;
+var myTable;
+var nums = [];
+
+function initlization() {
+  myTable = document.getElementsByTagName("table")[0];
+  shuffle(); //need to shuffle initially 
+}
+
+function shuffle() {
+  //need to get all tds each time the table is shuffled
+  blocks = document.getElementsByTagName("td");
+  tableSize = blocks.length;
+  shuffledArray(0, tableSize);
+  for (var i = 0; i < tableSize; i++) {
+    blocks[i].innerHTML = '<span draggable="true">' + nums[i] + '</span>';
+  }
+}
+function shuffledArray( from,  to) {
+for(i=from; i<to;i++){
+    nums[i-from]=i;
+    }
+for (var i = to-from; i; i--) {
+    j= Math.floor(Math.random() *i);
+
+    [nums[i - 1], nums[j]] = [nums[j], nums[i - 1]];
+    }
+}
